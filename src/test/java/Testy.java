0c8266e0ded3b9d4f7fd180edb1408261df09804
unknown <tomasz.sochacki@billennium.com>
2021-10-14 15:36:58 +0200

    Next step in test

--- src/test/java/Testy.java
+++ src/test/java/Testy.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,11 +24,11 @@ public class Testy {
 
     @Test
     public void mediaExpertTest() {
-        WebDriverWait wait = new WebDriverWait(webDriver, 20);
+        WebDriverWait wait = new WebDriverWait(webDriver, 5);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[class='spark-button is-primary is-default icon-left']")));
         this.webDriver.findElement(By.cssSelector("[class='spark-button is-primary is-default icon-left']")).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Telewizory i RTV")));
-        this.webDriver.findElement(By.linkText("Telewizory i RTV")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("TV, Audio i RTV")));
+        this.webDriver.findElement(By.linkText("TV, Audio i RTV")).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div/ul[@class='category']")));
         /*wait.until(ExpectedConditions.elementToBeClickable(By.className("icon close-icon icon-close")));
         this.webDriver.switchTo()
@@ -35,10 +36,23 @@ public class Testy {
         this.webDriver.findElement(By.className("icon close-icon icon-close")).click();
         this.webDriver.switchTo().defaultContent();*/
         this.webDriver.findElement(By.xpath("//div/img[@alt='Telewizory']")).click();
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("[class='offer-box']")));
-        Assertions.assertThat("30").isEqualTo(webDriver.findElements(By.cssSelector("[class='offer-box']")).size());
+        try {
+            wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("[class='offer-box']")));
+        } catch (StaleElementReferenceException e) {
+            System.out.printf("e");
+        }
+        Assertions.assertThat(this.webDriver.findElements(By.cssSelector("[class='offer-box']")).size()).isEqualTo(30);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[contains(text(),  '30 produktów')]")));
+        this.webDriver.findElement(By.xpath("//span[contains(text(),  '30 produktów')]")).click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(".//span[contains(text(),  '30 produktów')]")));
+        this.webDriver.findElement(By.xpath(".//span[contains(text(),  '30 produktów')]")).click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='sidebar']")));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(".//div/a[@title='Telewizory LED - RODZAJ TELEWIZORA: LCD']")));
+        this.webDriver.findElement(By.xpath(".//div/a[@title='Telewizory LED - RODZAJ TELEWIZORA: LCD']")).click();
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@uid='47352167']")));
+        Assertions.assertThat(this.webDriver.findElements(By.xpath("//div[@uid='47352167']")).size()).isEqualTo(3);
     }
-    
+
     @After
     public void tearDown() {
         this.webDriver.close();
