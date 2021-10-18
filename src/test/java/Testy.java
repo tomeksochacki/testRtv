c494319e7af8fed257a5d0e1860aa4dc4397e9be
tomek <tomeksochacki@wp.pl>
2021-10-18 23:44:24 +0200

    Screenshot_list_drag_and_drop test

--- src/test/java/Testy.java
+++ src/test/java/Testy.java
@@ -1,16 +1,21 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Testy {
     private WebDriver webDriver;
@@ -59,13 +64,13 @@ public class Testy {
         Assertions.assertThat(this.webDriver.findElements(By.cssSelector("[class='offer-box']")).size()).isEqualTo(3);
     }*/
 
-    /*@Test
+   /* @Test
     public void selectMethod(){
         this.webDriver.navigate().to("http://how2html.pl/select-html/");
         new Select(this.webDriver.findElement(By.id("dessert"))).selectByIndex(2);
     }*/
 
-    @Test
+    /*@Test
     public void dropDownProduct(){
         this.webDriver.navigate().to("https://www.samsung.com/pl/tvs/all-tvs/");
         WebDriverWait wait = new WebDriverWait(this.webDriver, 10);
@@ -74,10 +79,31 @@ public class Testy {
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("pf-finder-v2__filters-bar-select")));
         this.webDriver.findElement(By.className("pf-finder-v2__filters-bar-select")).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector("[style='max-height: 288px; transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s;']")));
-        this.webDriver.findElements(By.cssSelector("[style='max-height: 288px; transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s;'] li")).get(2).click();
+        Random random = new Random();
+        int randomNumber = random.nextInt(this.webDriver.findElements(By.cssSelector("[style='max-height: 288px; transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s;'] li")).size());
+        this.webDriver.findElements(By.cssSelector("[style='max-height: 288px; transition: max-height 0.2s cubic-bezier(0.4, 0, 0.2, 1) 0s;'] li")).get(randomNumber).click();
         wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")));
-        Assertions.assertThat(this.webDriver.findElements(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")).get(0).getText()).contains("98” QLED Q950R");
-        System.out.println(this.webDriver.findElements(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")).get(0).getText());
+        //Assertions.assertThat(this.webDriver.findElements(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")).get(0).getText()).contains("98” QLED Q950R");
+        //System.out.println(this.webDriver.findElements(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")).get(0).getText());
+        Assertions.assertThat(this.webDriver.findElements(By.cssSelector(".product-card-v2__item .product-card-v2__name-link")).size()).isEqualTo(8);
+    }*/
+
+    @Test
+    public void testyDragAndDrop() {
+        this.webDriver.navigate().to("https://marcojakob.github.io/dart-dnd/basic/");
+        Actions actions = new Actions(this.webDriver);
+        List<WebElement> list = this.webDriver.findElements(By.className("document"));
+        WebElement trash = this.webDriver.findElement(By.className("trash"));
+        for (int i = 0; i<list.size(); i++){
+            actions.dragAndDrop(list.get(i), trash).perform();
+        }
+        Assertions.assertThat(this.webDriver.findElement(By.cssSelector(".trash.full")).isEnabled()).isEqualTo(true);
+        File file = ((TakesScreenshot)this.webDriver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(file, new File("C:\\Desktop\\testphoto.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /*@After
